1e4e3d3be447da96f3a5d7140afdb77a8e1e9ae2
Björn <bjoern.n@mailbox.org>
2024-04-22 15:50:47 +0200

    change function for bookmarks

--- js/index.js
+++ js/index.js
@@ -6,89 +6,96 @@ const bookmarkImage5 = document.getElementById("bookmark-image5");
 const bookmarkImage6 = document.getElementById("bookmark-image6");
 
 let toggle = true;
-bookmarkImage1.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmarkImage1.src = "./resources/bookmark.png";
-  } else {
-    bookmarkImage1.src = "./resources/bookmark_filled.png";
-  }
+// bookmarkImage1.addEventListener("click", () => {
+//   toggle = !toggle;
+//   if (toggle) {
+//     bookmarkImage1.src = "./resources/bookmark.png";
+//   } else {
+//     bookmarkImage1.src = "./resources/bookmark_filled.png";
+//   }
+// });
+// bookmarkImage2.addEventListener("click", () => {
+//   toggle = !toggle;
+//   if (toggle) {
+//     bookmarkImage2.src = "./resources/bookmark.png";
+//   } else {
+//     bookmarkImage2.src = "./resources/bookmark_filled.png";
+//   }
+// });
+
+// bookmarkImage3.addEventListener("click", () => {
+//   toggle = !toggle;
+//   if (toggle) {
+//     bookmarkImage3.src = "./resources/bookmark.png";
+//   } else {
+//     bookmarkImage3.src = "./resources/bookmark_filled.png";
+//   }
+// });
+
+// bookmarkImage4.addEventListener("click", () => {
+//   toggle = !toggle;
+//   if (toggle) {
+//     bookmarkImage4.src = "./resources/bookmark.png";
+//   } else {
+//     bookmarkImage4.src = "./resources/bookmark_filled.png";
+//   }
+// });
+
+// bookmarkImage5.addEventListener("click", () => {
+//   toggle = !toggle;
+//   if (toggle) {
+//     bookmarkImage5.src = "./resources/bookmark.png";
+//   } else {
+//     bookmarkImage5.src = "./resources/bookmark_filled.png";
+//   }
+// });
+
+// bookmarkImage6.addEventListener("click", () => {
+//   toggle = !toggle;
+//   if (toggle) {
+//     bookmarkImage6.src = "./resources/bookmark.png";
+//   } else {
+//     bookmarkImage6.src = "./resources/bookmark_filled.png";
+//   }
+// });
+
+// 2nd version also possible but only for first bookmark
+
+const bookmarkImages = document.querySelectorAll(".question-card__bookmark");
+
+console.log("----------------", bookmarkImages);
+
+bookmarkImages.forEach((bookmark) => {
+  console.log(bookmark);
+  bookmark.addEventListener("click", (event) => {
+    console.log(event);
+    if (bookmark.src.match("./resources/bookmark.png")) {
+      bookmark.src = "./resources/bookmark_filled.png";
+    } else {
+      bookmark.src = "./resources/bookmark.png";
+    }
+  });
 });
-bookmarkImage2.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmarkImage2.src = "./resources/bookmark.png";
-  } else {
-    bookmarkImage2.src = "./resources/bookmark_filled.png";
-  }
-});
 
-bookmarkImage3.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmarkImage3.src = "./resources/bookmark.png";
-  } else {
-    bookmarkImage3.src = "./resources/bookmark_filled.png";
-  }
-});
-
-bookmarkImage4.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmarkImage4.src = "./resources/bookmark.png";
-  } else {
-    bookmarkImage4.src = "./resources/bookmark_filled.png";
-  }
-});
-
-bookmarkImage5.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmarkImage5.src = "./resources/bookmark.png";
-  } else {
-    bookmarkImage5.src = "./resources/bookmark_filled.png";
-  }
-});
-
-bookmarkImage6.addEventListener("click", () => {
-  toggle = !toggle;
-  if (toggle) {
-    bookmarkImage6.src = "./resources/bookmark.png";
-  } else {
-    bookmarkImage6.src = "./resources/bookmark_filled.png";
-  }
-});
-
-/*--------------------------------------------------------------------
-
-2nd version also possible but only for first bookmark
-
-
-function changeBookmarkImages() {
-  const bookmarkImage = document.getElementsByClassName(
-    "question-card__bookmark"
-  );
-  if (bookmarkImage.src.match("./resources/bookmark.png")) {
-    bookmarkImage.src = "./resources/bookmark_filled.png";
-  } else {
-    bookmarkImage.src = "./resources/bookmark.png";
-  }
-}
-
-------------------------------------------------------------------------*/
 let isClicked = false;
 
 function answerButton() {
+  console.log(event);
   const answer = document.querySelector('[data-js="question-card__answer"]');
   const button = document.querySelector(
     '[data-js="question-card__answer-button"]'
   );
+
+  console.log(button);
+
   isClicked = answer.style.display === "none";
   if (isClicked) {
     answer.style.display = "block";
+    console.log("if", button);
     button.textContent = "Hide answer";
   } else {
     answer.style.display = "none";
+    console.log("else");
     button.textContent = "Show answer";
   }
 }
