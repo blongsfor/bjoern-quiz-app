2a19087d2f5a2ba4630f1535aa1ecd07030e8e33
Björn <bjoern.n@mailbox.org>
2024-04-23 11:14:39 +0200

    fix submit question function

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const formMain = document.querySelector('[data-js="form-main"]');
+const formMain = document.querySelector('[data-js="form-output"]');
 const form = document.querySelector('[data-js="form"]');
 const formSubmitButton = document.querySelector('[data-js="submit-Button"]');
 const formYourQuestion = document.querySelector('[data-js="your-question"]');
@@ -11,11 +11,11 @@ const answerCharacterCount = document.querySelector(
   '[data-js="your-answer-character-count"]'
 );
 
-console.log("hallo");
+console.log("hallo", button);
 function createNewQuestion(event) {
+  console.log("Button clicked!");
   event.preventDefault();
 
-  console.log("Button clicked!");
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
   console.log(data);
@@ -30,27 +30,40 @@ function createNewQuestion(event) {
 
   const question = document.createElement("p");
   question.classList.add("question-card__question");
+  question.textContent = data.yourQuestion;
+
+  const answer = document.createElement("p");
+  answer.classList.add("question-card__answer");
+  answer.textContent = data.yourAnswer;
+
+  const tag = document.createElement("p");
+  tag.textContent = data.submitTag;
+  tag.classList.add("question-card__tag-list__tag");
 
   const button = document.createElement("button");
   button.classList.add("question-card__answer-button");
   button.setAttribute("data-js", "question-card__answer-button");
   button.textContent = "Show answer";
+  button.addEventListener("click", (event) => {
+    const closestAnswer = event.target
+      .closest(".question-card")
+      .querySelector(".question-card__answer");
 
-  const answer = document.createElement("p");
-  question.classList.add("question-card__answer");
-
-  const tag = document.createElement("p");
-  tag.classList.add("question-card__tag-list__tag");
-
+    if (closestAnswer.style.display === "none") {
+      closestAnswer.style.display = "block";
+      event.target.textContent = "Hide Answer"; // Access clicked button directly
+    } else {
+      closestAnswer.style.display = "none";
+      event.target.textContent = "Show Answer"; // Access clicked button directly
+    }
+  });
+  //newQuestionSection.append(img);
+  newQuestionSection.append(question, answer, tag, button);
   formMain.append(newQuestionSection);
-  newQuestionSection.append(img);
-  newQuestionSection.append(question);
-  newQuestionSection.append(answer);
-  newQuestionSection.append(button);
 
   event.target.reset();
 }
-button.addEventListener("submit", createNewQuestion);
+form.addEventListener("submit", createNewQuestion);
 
 // -------- Character Count ---------------------------
 
